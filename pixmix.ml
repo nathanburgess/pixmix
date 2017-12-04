47c0274f61250872e32b2a822a9353006ad94b31
Edvard <edvardhceriksson@gmail.com>
2017-12-04 10:25:06 -0500

    changed usage message

--- pixmix.ml
+++ pixmix.ml
@@ -14,7 +14,7 @@ let _ =
         ("-c", Arg.Unit (set_action Compile),
          "Check and print the generated LLVM IR (default)");
     ] in  
-    let usage_msg = "usage: ./pixmix.native [-a|-l|-c] [file.mc]" in
+    let usage_msg = "usage: ./pixmix.native [-a|-l|-c] [file.pm]" in
     let channel = ref stdin in
     Arg.parse speclist (fun filename -> channel := open_in filename) usage_msg;
     let lexbuf = Lexing.from_channel !channel in
