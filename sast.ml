e02aca6dd67001117cdb595e137694adf5e1ea93
Edvard <edvardhceriksson@gmail.com>
2017-12-06 17:53:28 -0500

    Added skeleton for sast

--- sast.ml
+++ sast.ml
@@ -2,3 +2,15 @@ module A = Ast
 
 let convert ast = function
     | [] -> []
+
+type sexpr =
+    |(* List expressions to resolve *)
+
+type sstmt =
+    | (*List statements to resolve *)
+
+type sfuncdecl =
+    | (* List function declarations to resolve *)
+
+type sprogram =
+    | (*List program components to resolve *)
