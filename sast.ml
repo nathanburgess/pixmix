f82a0de92f0a75ea8e7d68ed7d200c8f6559b1fe
Edvard <edvardhceriksson@gmail.com>
2017-12-06 17:55:03 -0500

    minor edit

--- sast.ml
+++ sast.ml
@@ -12,5 +12,8 @@ type sstmt =
 type sfuncdecl =
     | (* List function declarations to resolve *)
 
+type sobjdecl =
+    | (*List object declaration components to resolve *)
+
 type sprogram =
     | (*List program components to resolve *)
