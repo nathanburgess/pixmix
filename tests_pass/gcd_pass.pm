8206261e7367575b6fdd3a10a99a272f133e77ec
Allie Taylor <at3022@columbia.edu>
2017-12-19 18:20:24 +0000

    gcd test

---
+++ tests_pass/gcd_pass.pm
@@ -0,0 +1,18 @@
+num gcd (num x, num y)
+{
+  while ( x != y) {
+  if ( x > y) { x = x - y;}
+  else {
+  y = y - x; }
+  
+  return x;
+}
+
+}
+
+
+  
+ 
+  print(gcd(2, 3));
+  print("finished");
+  
